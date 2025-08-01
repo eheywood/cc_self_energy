a02d44ef1f52c78ef744d568cb81cd8d83eb2010
ellie <ellie@WSLUbuntu.com>
2025-08-01 15:34:15 +0100

    fixed silly mistakes

--- cc_BSE.py
+++ cc_BSE.py
@@ -5,6 +5,40 @@ import BSE_Helper as bse
 np.set_printoptions(precision=6, suppress=True, linewidth=100000)
 eV_to_Hartree = 0.0367493
 
+def build_gfock(t2, oovv, fock_occ, fock_vir,n_occ,n_vir) -> tuple[np.ndarray,np.ndarray]:
+    # n_occ x n_occ, n_vir x n_vir
+    occ_selfeng, vir_selfeng = bse.get_self_energy(t2,oovv)
+
+    # n_occ x n_occ, n_vir x n_vir
+    fock_occ, fock_vir = bse.build_fock_matrices(mol,n_occ,n_vir) 
+
+    F_ij = occ_selfeng + fock_occ
+    F_ab = vir_selfeng + fock_vir
+
+    return F_ij,F_ab
+
+def build_bse(F_ij:np.ndarray,F_ab,ovvo,goovv,t2, n_occ, n_vir) -> np.ndarray:
+    
+    F_abij = np.einsum('ab, ij -> iajb', F_ab, np.identity(n_occ),optimize='optimal')
+    F_ijab = np.einsum('ij, ab -> iajb', F_ij, np.identity(n_vir),optimize='optimal')
+
+    nspincase = 4
+    H_bse = np.zeros((n_occ,n_vir,n_occ,n_vir,nspincase))
+    
+    for i in range(4):
+        H_bse[:,:,:,:,i] = F_abij - F_ijab + np.einsum("iabj->iajb", ovvo,optimize='optimal')
+
+        for ispina in range(2):
+            for ispinb in range (2):
+                if ispina == ispinb:
+                    # ispincase = 1  or 2
+                    H_bse[:,:,:,:,i] += - np.einsum("ikcb, jkca -> iajb", goovv,t2,optimize="optimal")
+    
+                else:
+                    # ispincase = 3 or 4
+                    H_bse[:,:,:,:,i] += - np.einsum("ikbc, jkac -> iajb", goovv,t2,optimize="optimal") + np.einsum("ikbc, jkca -> iajb", oovv,t2,optimize="optimal")
+    return H_bse
+
 if __name__ == "__main__":
 
     # Define Molecule to calculate amplitudes and mo for
@@ -21,12 +55,13 @@ if __name__ == "__main__":
     n_vir = vir_spinorbs.shape[1]
 
     # Build electron repulsion integrals
-    _, eri_ao = bse.spinor_one_and_two_e_int(mol)                       # Find eri in spinor form
+    _, eri_ao = bse.spinor_one_and_two_e_int(mol)                   # Find eri in spinor form
     eri_ao = np.einsum("pqrs->prqs", eri_ao, optimize="optimal")    # Convert to Physicist's notation
     anti_eri_ao = eri_ao - np.einsum("prqs->prsq", eri_ao, optimize='optimal')
 
     # antisymmetrised:
     oovv,ooov,vovv,ovvo,ovov = bse.build_double_ints(core_spinorbs,vir_spinorbs,anti_eri_ao)
+    goovv = np.einsum("pi,qa,pqrs,rj,sb->iajb", core_spinorbs, core_spinorbs, eri_ao, vir_spinorbs, vir_spinorbs, optimize="optimal")
 
     # n_occ x n_occ, n_vir x n_vir
     occ_selfeng, vir_selfeng = bse.get_self_energy(t2,oovv)
@@ -35,13 +70,17 @@ if __name__ == "__main__":
     fock_occ, fock_vir = bse.build_fock_matrices(mol,n_occ,n_vir)
 
     # n_occ x n_occ, n_vir x n_vir
-    gfock_occ, gfock_vir = build_gfock(t2, oovv, fock_occ, fock_vir)
+    gfock_occ, gfock_vir = build_gfock(t2, oovv, fock_occ, fock_vir,n_occ,n_vir)
 
     # (n_occ,n_vir,n_occ,n_vir,nspincase)
-    hbse = build_bse(occ_selfeng, vir_selfeng, gfock_occ, gfock_vir, n_occ, n_vir)
+    hbse = build_bse(gfock_occ,gfock_vir,ovvo,goovv,t2, n_occ, n_vir)
     
-    #H_bse = H_bse.reshape((n_occ*n_vir,n_occ*n_vir))
-    #e, v = np.linalg.eig(hbse)
+    H_bse_aaaa = hbse[:,:,:,:,0].reshape((n_occ*n_vir,n_occ*n_vir))
+    H_bse_bbbb = hbse[:,:,:,:,1].reshape((n_occ*n_vir,n_occ*n_vir))
+    H_bse_abab = hbse[:,:,:,:,2].reshape((n_occ*n_vir,n_occ*n_vir))
+    H_bse_baba = hbse[:,:,:,:,3].reshape((n_occ*n_vir,n_occ*n_vir))
+
+    e, v = np.linalg.eig(H_bse_abab)
 
     np.savetxt("eigenvals.csv", np.sort(np.real(e)), delimiter=',')
 
